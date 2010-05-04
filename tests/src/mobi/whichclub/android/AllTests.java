263e2f65cc364dffa79590fba627ac7b8dc668a9
Cameron Dale <camrdale@gmail.com>
2010-05-03 20:55:01 -0700

    Add a test suite to run all the android unit tests

---
+++ tests/src/mobi/whichclub/android/AllTests.java
@@ -0,0 +1,11 @@
+package mobi.whichclub.android;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(AllTests.class).includeAllPackagesUnderHere().build();
+    }
+}
